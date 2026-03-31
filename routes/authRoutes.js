d69893748e04dfd69a8f28c36f48582ff149a166
ericstober <erictstober@gmail.com>
2026-03-31 10:48:23 -0500

    add refresh route and use refresh token to get new access token

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import User from "../models/User.js";
+import { jwtVerify } from "jose";
+import { JWT_SECRET } from "../utils/getJWTSecret.js";
 import generateToken from "../utils/generateToken.js";
 
 const router = express.Router();
@@ -118,6 +120,44 @@ router.post("/logout", (req, res) => {
   });
 
   res.status(200).json({ message: "Logged out successfully" });
+});
+
+// @route POST apit/auth/refresh
+// @description Generate new access token from refresh token
+// @access Public (needs valid refresh token in cookie)
+router.post("/refresh", async (req, res, next) => {
+  try {
+    const token = req.cookies?.refreshToken;
+    console.log("Refreshing token...");
+
+    if (!token) {
+      res.status(401);
+      throw new Error("No refresh token");
+    }
+
+    const { payload } = await jwtVerify(token, JWT_SECRET);
+
+    const user = await User.findById(payload.userId);
+
+    if (!user) {
+      res.status(401);
+      throw new Error("No user");
+    }
+
+    const newAccessToken = await generateToken({ userId: user._id.toString() }, "1m");
+
+    res.json({
+      accessToken: newAccessToken,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(401);
+    next(error);
+  }
 });
 
 export default router;
