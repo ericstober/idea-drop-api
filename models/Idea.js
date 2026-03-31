47c18258ae69cdc2028a2035786555e2edb3ee68
ericstober <erictstober@gmail.com>
2026-03-31 11:54:57 -0500

    add route protection middlware

--- models/Idea.js
+++ models/Idea.js
@@ -5,7 +5,6 @@ const ideaScheme = new mongoose.Schema(
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
     },
     title: {
       type: String,
--- routes/ideaRoutes.js
+++ routes/ideaRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Idea from "../models/Idea.js";
 import mongoose from "mongoose";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
@@ -53,7 +54,7 @@ router.get("/:id", async (req, res, next) => {
 // @route POST /api/ideas
 // @description Create new idea
 // @access Public
-router.post("/", async (req, res, next) => {
+router.post("/", protect, async (req, res, next) => {
   try {
     const { title, summary, description, tags } = req.body || {};
 
@@ -88,7 +89,7 @@ router.post("/", async (req, res, next) => {
 // @route DELETE /api/ideas/:id
 // @description Delete idea
 // @access Public
-router.delete("/:id", async (req, res, next) => {
+router.delete("/:id", protect, async (req, res, next) => {
   try {
     const { id } = req.params;
 
@@ -113,7 +114,7 @@ router.delete("/:id", async (req, res, next) => {
 // @route PUT /api/ideas/:id
 // @description Update idea
 // @access Public
-router.put("/:id", async (req, res, next) => {
+router.put("/:id", protect, async (req, res, next) => {
   try {
     const { id } = req.params;
 
