19948532ed3a818ab5a43d1ded3320c30dcac308
ericstober <erictstober@gmail.com>
2026-03-31 09:21:48 -0500

    add logout route & clear refresh token cookie

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -52,4 +52,17 @@ router.post("/register", async (req, res, next) => {
   }
 });
 
+// @route POST apit/auth/logout
+// @description Logout user and clear refresh token
+// @access Private
+router.post("/logout", (req, res) => {
+  res.clearCookie("refreshToken", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
+  });
+
+  res.status(200).json({ message: "Logged out successfully" });
+});
+
 export default router;
